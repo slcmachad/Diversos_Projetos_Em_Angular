9b6ffcae6e32572db0973d96e54478fac094ed41
Salomao <salomao.machado.75@gmail.com>
2023-08-02 22:51:55 -0300

    <feat> criando metodos utilizados no projeto

--- project-001-border-radius-previewer/src/app/border-radius-previewer/border-radius-previewer.component.ts
+++ project-001-border-radius-previewer/src/app/border-radius-previewer/border-radius-previewer.component.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./border-radius-previewer.component.css']
 })
 export class BorderRadiusPreviewerComponent {
-borderRadiusStyle: any;
-borderRadius: any;
+
+  borderRadius = {
+    topLeft: 0,
+    topRight: 0,
+    bottomLeft: 0,
+    bottomRight: 0,
+  };
+
+  borderRadiusStyle = '0';
+
+  updateCSS() {
+    this.borderRadiusStyle = `${this.borderRadius.topLeft}px
+                              ${this.borderRadius.topRight}px
+                              ${this.borderRadius.bottomLeft}px
+                              ${this.borderRadius.bottomRight}px`
+  }
 
 }
