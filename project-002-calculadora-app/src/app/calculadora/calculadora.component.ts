7740f55a3700d886dc75eb9ffc1c3829e37d0694
Salomao <salomao.machado.75@gmail.com>
2023-08-04 20:25:30 -0300

    <feat> Adicionando as formulas para as operações

--- project-002-calculadora-app/src/app/calculadora/calculadora.component.ts
+++ project-002-calculadora-app/src/app/calculadora/calculadora.component.ts
@@ -8,29 +8,54 @@ import { Component } from '@angular/core';
 export class CalculadoraComponent {
 
   displayValue = '0';
-  currentInput = '0';
-  currentOperator = '';
+  inputAtual = '0';
+  operadorAtual = '';
   resultado = 0;
 
   appendToDisplay(value: string): void{
-    if(this.currentInput.length < 8){
-      if(this.currentInput === '0' && value !== '.'){
-        this.currentInput = value;
+    if(this.inputAtual.length < 8){
+      if(this.inputAtual === '0' && value !== '.'){
+        this.inputAtual = value;
       }else {
-        this.currentInput += value;
+        this.inputAtual += value;
       }
-      this.displayValue = this.currentInput;
+      this.displayValue = this.inputAtual;
     }
   }
 
-
   performOperation(operator: string): void{
     this.calcular();
-    this.currentOperator = operator;
-    this.currentInput = '0';
+    this.operadorAtual = operator;
+    this.inputAtual = '0';
   }
 
   calcular(): void{
+    const valorAtual = parseFloat(this.inputAtual);
+
+    switch(this.operadorAtual){
+      case '+':
+        this.resultado += valorAtual;
+        break;
+      case '-':
+        this.resultado -= valorAtual;
+        break;
+      case '*':
+        this.resultado *= valorAtual;
+        break;
+      case '/':
+        if(valorAtual === 0){
+          this.displayValue = 'ERR';
+          this.resetarCalculadora();
+          return
+        }
+        this.resultado /= valorAtual;
+        break;
+      default:
+        this.resultado = valorAtual;
+        break;
+    }
+  }
+  resetarCalculadora() {
     throw new Error('Method not implemented.');
   }
 
